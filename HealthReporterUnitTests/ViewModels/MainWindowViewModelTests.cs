88e1ef14e7f5d71b95f2c18ef0e0b1c3fc5d548d
Sander <sander.sats@gmail.com>
2016-11-07 00:34:47 +0200

    Added UI test for adding client groups

--- HealthReporterUnitTests/ViewModels/MainWindowViewModelTests.cs
+++ HealthReporterUnitTests/ViewModels/MainWindowViewModelTests.cs
@@ -1,6 +1,13 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using HealthReporter;
+using TestStack.White.UIItems.MenuItems;
+using TestStack.White.UIItems;
+using TestStack.White;
+using System.IO;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.Factory;
+using TestStack.White.UIItems.Finders;
 
 // https://msdn.microsoft.com/en-us/library/ms182532.aspx
 
@@ -9,6 +16,14 @@ namespace HealthReporterUnitTests
     [TestClass]
     public class MainWindowViewModelTests
     {
+        private TestContext _testContext;
+
+        public TestContext TestContext
+        {
+            get { return _testContext; }
+            set { _testContext = value; }
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
@@ -22,6 +37,37 @@ namespace HealthReporterUnitTests
 
             // assert
             Assert.AreEqual(expectedSum, actualSum, 0.001, "Wrong sum");
+        }
+
+        [TestMethod]
+        public void TestAppWhite()
+        {
+            string groupName = "Test grupp";
+            string solutionDir = Path.GetDirectoryName(Path.GetDirectoryName(TestContext.TestRunDirectory));
+            string applicationPath = Path.Combine(solutionDir, "HealthReporter/bin/Debug/HealthReporter.exe");
+            Application application = Application.Launch(applicationPath);
+            Window window = application.GetWindow("Health Reporter", InitializeOption.NoCache);
+
+            ListView groups = window.Get<ListView>("groupDataGrid");
+            var rows = groups.Rows;
+            int groupCount = rows.Count;
+
+            Button button = window.Get<Button>("addStuff");
+            button.Click();
+
+            PopUpMenu menu = window.Popup;
+            menu.Item("New group").Click();
+
+
+            groups.SelectedRows[0].Enter(groupName);
+            window.Click();
+
+            rows = groups.Rows;
+            int newCount = rows.Count;
+            string newName = rows[newCount - 1].Name;
+            var test = rows[newCount - 1].GetElement(SearchCriteria.All);
+            application.Close();
+            Assert.AreEqual(groupCount + 1, newCount, 0, "Wrong number of groups after adding one.");
         }
     }
 }
