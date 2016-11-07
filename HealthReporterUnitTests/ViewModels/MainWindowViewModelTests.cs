e100a9a1b10f148865f2b100409557cc0a3ba41f
Sander <sander.sats@gmail.com>
2016-11-07 02:06:05 +0200

    #52 added addClient UI test

--- HealthReporterUnitTests/ViewModels/MainWindowViewModelTests.cs
+++ HealthReporterUnitTests/ViewModels/MainWindowViewModelTests.cs
@@ -8,6 +8,7 @@ using System.IO;
 using TestStack.White.UIItems.WindowItems;
 using TestStack.White.Factory;
 using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.WPFUIItems;
 
 // https://msdn.microsoft.com/en-us/library/ms182532.aspx
 
@@ -24,23 +25,23 @@ namespace HealthReporterUnitTests
             set { _testContext = value; }
         }
 
+        //[TestMethod]
+        //public void TestMethod1()
+        //{
+        //    // arrange
+        //    double a = 11.99;
+        //    double b = 12;
+        //    double expectedSum = a+b;
+
+        //    // act
+        //    double actualSum = a + b;
+
+        //    // assert
+        //    Assert.AreEqual(expectedSum, actualSum, 0.001, "Wrong sum");
+        //}
+
         [TestMethod]
-        public void TestMethod1()
-        {
-            // arrange
-            double a = 11.99;
-            double b = 12;
-            double expectedSum = a+b;
-
-            // act
-            double actualSum = a + b;
-
-            // assert
-            Assert.AreEqual(expectedSum, actualSum, 0.001, "Wrong sum");
-        }
-
-        [TestMethod]
-        public void TestAppWhite()
+        public void TestAddClientGroup()
         {
             string groupName = "Test grupp";
             string solutionDir = Path.GetDirectoryName(Path.GetDirectoryName(TestContext.TestRunDirectory));
@@ -68,6 +69,53 @@ namespace HealthReporterUnitTests
             var test = rows[newCount - 1].GetElement(SearchCriteria.All);
             application.Close();
             Assert.AreEqual(groupCount + 1, newCount, 0, "Wrong number of groups after adding one.");
+        }
+
+        [TestMethod]
+        public void TestAddClient()
+        {
+            string solutionDir = Path.GetDirectoryName(Path.GetDirectoryName(TestContext.TestRunDirectory));
+            string applicationPath = Path.Combine(solutionDir, "HealthReporter/bin/Debug/HealthReporter.exe");
+            Application application = Application.Launch(applicationPath);
+            Window window = application.GetWindow("Health Reporter", InitializeOption.NoCache);
+
+            ListView groups = window.Get<ListView>("groupDataGrid");
+            Button addButton = window.Get<Button>("addStuff");
+            PopUpMenu menu;
+            if (groups.Rows.Count <= 0)
+            {
+                addButton.Click();
+
+                menu = window.Popup;
+                menu.Item("New group").Click();
+
+
+                groups.SelectedRows[0].Enter("Test grupp 2");
+                window.Click();
+            }
+
+            groups.Rows[0].Click();
+
+            ListView clients = window.Get<ListView>("clientDataGrid");
+            int clientCount = clients.Rows.Count;
+
+            addButton.Click();
+            menu = window.Popup;
+
+            menu.Item("New client").Click();
+
+            TextBox firstName = window.Get<TextBox>("firstName");
+            firstName.Enter("Test eesnimi");
+            TextBox lastName = window.Get<TextBox>("lastName");
+            lastName.Enter("Test perenimi");
+            TextBox email = window.Get<TextBox>("email");
+            email.Enter("test@test.ee");
+
+            groups.Rows[0].Click();
+
+            int newClientCount = clients.Rows.Count;
+            application.Close();
+            Assert.AreEqual(clientCount + 1, newClientCount, 0, "Wrong number of clients after adding one.");
         }
     }
 }
